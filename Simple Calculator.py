c413153f3b335895df4886ecabba9c09274c5bc2
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-03-31 09:36:51 -0700

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,17 @@
+num_1 = int(input("Entr 1st number: "))
+opt = input("Enter operator: ")
+num_2 = int(input("Enter 2nd number: "))
+if opt == "+":
+    print(num_1,opt,num_2,"=",num_1 + num_2)
+if opt == "-":
+    print(num_1,opt,num_2,"=",num_1 - num_2)
+if opt == "*":
+    print(num_1,opt,num_2,"=",num_1 * num_2)
+if opt == "/":
+    print(num_1,opt,num_2,"=",num_1 / num_2)
+if opt == "**":
+    print(num_1,opt,num_2,"=",num_1 ** num_2)
+if opt == "%":
+    print(num_1,opt,num_2,"=",num_1 % num_2)
+
+       
