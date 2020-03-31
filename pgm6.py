e55ed3236a04a966757e4bd3a9ca069020467e74
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-03-31 05:17:41 -0700

    Delete pgm6.py

--- pgm6.py
+++
@@ -1,5 +0,0 @@
-a = int(input("How much shopping have you done?\n"))
-b = int(input("\nHow much have you paid?\n"))
-c = b-a
-print("-----------------------")
-print("Your bill =",a,"\nAmount you paid =",b,"\nReturn amount =",c)
