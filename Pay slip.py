95ceca4de2e4e308d640d4cc49c479a6c5945535
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-03-31 05:18:29 -0700

    Add files via upload

---
+++ Pay slip.py
@@ -0,0 +1,5 @@
+a = int(input("How much shopping have you done?\n"))
+b = int(input("\nHow much have you paid?\n"))
+c = b-a
+print("-----------------------")
+print("Your bill =",a,"\nAmount you paid =",b,"\nReturn amount =",c)
