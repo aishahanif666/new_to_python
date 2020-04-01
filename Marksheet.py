60e37dd2b3acc84d7521fdc5af093e7a4da04aa5
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-04-01 00:18:17 -0700

    Add files via upload

---
+++ Marksheet.py
@@ -0,0 +1,10 @@
+print("MARKS OBTAINED: \n")
+a = int(input("Maths: "))
+b = int(input("Physics: "))
+c = int(input("Chemistry: "))
+d = int(input("English: "))
+e = int(input("Urdu: "))
+total_marks = 500
+marks_obtained = a+b+c+d+e
+print("\n--------------------")
+print("\nYour Percentage: ",(marks_obtained/total_marks)*100,"%")
