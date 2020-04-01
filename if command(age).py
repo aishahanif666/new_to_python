99d92744fbb611a6cc0347d1c49ee621fd2fae87
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-04-01 12:42:42 -0700

    Add files via upload

---
+++ if command(age).py
@@ -0,0 +1,11 @@
+a = input("YOUR NAME: ")
+b = input("YOUR FATHER'S NAME: ")
+c = int(input("YOUR AGE: "))
+if ((c >= 0) and (c <= 10)):
+    print("Your age is less than or equals to 10! ")
+if ((c > 10) and (c <= 20)):
+    print("Your age is greater than 10 and less than or equals to 20! ")
+if ((c >  20) and (c <= 30)):
+    print("Your age is greater than 20 and less than or equals to 30! ")
+    
+    
