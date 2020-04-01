6a21aa1d566e6251a7d7c03edbd27d726dd10ceb
Ayesha Hanif <62894932+aishahanif666@users.noreply.github.com>
2020-04-01 00:26:23 -0700

    Add files via upload

---
+++ Boolean operations practice.py
@@ -0,0 +1,19 @@
+check = (134>120) and (250==240) or (324>330) and (200!=150)
+"""(134>120) and (250==240)
+    True and False
+    false
+    (324>330) and (200!=150)
+    false and true
+    false
+    (250==240) or (324>330)
+    false or false = false"""
+print(check)
+check = (64>23) or (55<80) and (34==43) or (12<9)
+"""64>23 and 55<80
+   true and true = true
+   34==43 or 12<9
+   false or false = false
+   55<80 and 34==43
+   true and false = false"""
+print(check) 
+    
